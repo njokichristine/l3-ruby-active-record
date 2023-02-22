91ecc9833220ff08848f27e733c3f14bd73b0c55
njokichristine <christinenjoki6900@gmail.com>
2023-02-22 12:16:25 +0300

    code alongs

--- app/app.rb
+++ app/app.rb
@@ -2,21 +2,36 @@ require "pry"
 require "active_record"
 
 # TODO: CONNECT TO THE DATABASE
-
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/school.db"
+)
 # TODO: CREATE TABLE
+Create_tm_table = <<-SQL
+CREATE TABLE IF NOT EXISTS mentors
+(id INTEGER PRIMARY KEY, name TEXT, email TEXT, UNIQUE(email)
+)
+SQL
+
+ActiveRecord::Base.connection.execute(Create_tm_table)
+
 
 # TODO: Create class that associates with the db table
-
+class Mentor < ActiveRecord::Base
+end
 # TODO: VIEW METHODS AVAILABLE FOR TABLE (methods)
-
+# puts Mentor.methods
 # TODO: COLUMN NAMES (column_names)
-
+pp Mentor.column_names
 # TODO: CREATE NEW RECORD (create)
-
+# Mentor.create(name: "kelvin", email: "kelvin@moringaschool.com")
 # TODO: VIEW ALL RECORDS (all)
-
+pp Mentor.all
 # TODO: FIND BY ID (find)
-
+pp Mentor.find(1)
 # TODO: FIND BY GIVEN CONDITIONS (find_by)
-
+pp Mentor.find_by(name: "kelvin")
 # TODO: UPDATE RECORDS (save)
+kelvin = Mentor.find_by(name: "kelvin")
+kelvin.email = "kelvin1234@gmail.com"
+kelvin.save()
